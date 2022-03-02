5a141b640344099116e1df2c9eb42eb8403fc002
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-03-02 18:39:06 -0500

    Update bash script

--- collect_data_osx.sh
+++ collect_data_osx.sh
@@ -7,7 +7,7 @@ DAY="25"
 YEAR="2022"
 
 # A keyword that describes the qualities of the dataset
-DESCRIPTION="viam_3rd_floor"
+DESCRIPTION="outside"
 
 # This script works for osx only, and assumes the path to the rplidar is this:
 RPLIDAR_PATH=/dev/tty.SLAB_USBtoUART
@@ -18,3 +18,7 @@ RPLIDAR_PATH=/dev/tty.SLAB_USBtoUART
 DATA_DIRECTORY_NAME="data_${MONTH}_${DAY}_${YEAR}_${DESCRIPTION}"
 # collect data
 go run cmd/savepcdfiles/main.go -device $RPLIDAR_PATH -datafolder $DATA_DIRECTORY_NAME
+
+# If you have GDrive installed locally, move the data over to the cloud:
+DATA_BASE_DIRECTORY="$HOME/Shared drives/Engineering/SLAM/02 - SLAM V1/Data/RPLidar" 
+cp -r DATA_DIRECTORY_NAME DATA_BASE_DIRECTORY
