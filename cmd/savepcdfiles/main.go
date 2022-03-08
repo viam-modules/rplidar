704d372bb2b85d69c3026ee6c1ddd825295483a2
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2022-03-08 15:00:07 -0500

    Reduce duplicate code

--- cmd/savepcdfiles/main.go
+++ cmd/savepcdfiles/main.go
@@ -4,20 +4,17 @@ import (
 	"context"
 	"errors"
 	"math"
-	"os"
-	"path/filepath"
 	"time"
 
 	"go.uber.org/multierr"
 	"go.viam.com/rplidar"
+	"go.viam.com/rplidar/cmd/helper"
 
 	"github.com/edaniels/golog"
 	"go.viam.com/rdk/component/camera"
 	"go.viam.com/rdk/config"
-	"go.viam.com/rdk/metadata/service"
 	"go.viam.com/rdk/rlog"
 	"go.viam.com/rdk/robot"
-	"go.viam.com/rdk/usb"
 	"go.viam.com/utils"
 
 	robotimpl "go.viam.com/rdk/robot/impl"
@@ -25,7 +22,6 @@ import (
 
 var (
 	defaultTimeDeltaMilliseconds = 100
-	defaultPort                  = 8081
 	defaultDataFolder            = "data"
 	logger                       = rlog.Logger.Named("save_pcd_files")
 	name                         = "rplidar"
@@ -37,10 +33,9 @@ func main() {
 
 // Arguments for the command.
 type Arguments struct {
-	Port                  utils.NetPortFlag `flag:"0"`
-	TimeDeltaMilliseconds int               `flag:"delta,usage=delta ms"`
-	DevicePath            string            `flag:"device,usage=device path"`
-	DataFolder            string            `flag:"datafolder,usage=datafolder path"`
+	TimeDeltaMilliseconds int    `flag:"delta,usage=delta ms"`
+	DevicePath            string `flag:"device,usage=device path"`
+	DataFolder            string `flag:"datafolder,usage=datafolder path"`
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error {
@@ -49,68 +44,25 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 	if err := utils.ParseFlags(args, &argsParsed); err != nil {
 		return err
 	}
+	scanTimeDelta := helper.GetTimeDeltaMilliseconds(argsParsed.TimeDeltaMilliseconds, defaultTimeDeltaMilliseconds, logger)
 
-	if argsParsed.TimeDeltaMilliseconds == 0 {
-		logger.Debugf("using default time delta %d ", defaultTimeDeltaMilliseconds)
-		argsParsed.TimeDeltaMilliseconds = defaultTimeDeltaMilliseconds
-	} else {
-		logger.Debugf("using user defined time delta %d ", argsParsed.TimeDeltaMilliseconds)
-	}
-
-	if argsParsed.Port == 0 {
-		logger.Debugf("using default port %d ", defaultPort)
-		argsParsed.Port = utils.NetPortFlag(defaultPort)
-	} else {
-		logger.Debugf("using user defined port %d ", argsParsed.Port)
-	}
-
-	devicePath := argsParsed.DevicePath
-	if devicePath == "" {
-		usbDevices := usb.Search(
-			usb.SearchFilter{},
-			func(vendorID, productID int) bool {
-				return vendorID == rplidar.USBInfo.Vendor && productID == rplidar.USBInfo.Product
-			})
-
-		if len(usbDevices) != 0 {
-			logger.Debugf("detected %d lidar devices", len(usbDevices))
-			for _, comp := range usbDevices {
-				logger.Debug(comp)
-			}
-			devicePath = usbDevices[0].Path
-		} else {
-			return errors.New("no usb devices found")
-		}
-	}
-
-	if argsParsed.DataFolder == "" {
-		logger.Debugf("using default data folder '%s' ", defaultDataFolder)
-		argsParsed.DataFolder = defaultDataFolder
-	} else {
-		logger.Debugf("using user defined data folder %s", argsParsed.DataFolder)
-	}
-	if err := os.MkdirAll(filepath.Join(".", argsParsed.DataFolder), os.ModePerm); err != nil {
-		return errors.New("can not create a new directory named: " + argsParsed.DataFolder)
-	}
-
-	// Create rplidar component
-	lidarComponent := config.Component{
-		Name:  name,
-		Type:  config.ComponentTypeCamera,
-		Model: rplidar.ModelName,
-		Attributes: config.AttributeMap{
-			"device_path": devicePath,
-			"data_folder": argsParsed.DataFolder,
-		},
-	}
-
-	metadataSvc, err := service.New()
+	lidarDevice, err := helper.CreateRplidarComponent(name,
+		rplidar.ModelName,
+		argsParsed.DevicePath,
+		argsParsed.DataFolder,
+		defaultDataFolder,
+		config.ComponentTypeCamera,
+		logger)
 	if err != nil {
 		return err
 	}
-	ctx = service.ContextWithService(ctx, metadataSvc)
 
-	cfg := &config.Config{Components: []config.Component{lidarComponent}}
+	ctx, err = helper.GetServiceContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	cfg := &config.Config{Components: []config.Component{lidarDevice}}
 	myRobot, err := robotimpl.New(ctx, cfg, logger)
 	if err != nil {
 		return err
@@ -121,16 +73,7 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 		return errors.New("no rplidar found with name: " + name)
 	}
 
-	// Based on empirical data, we can see that the rplidar collects data at a rate of 15Hz,
-	// which is ~ 66ms per scan. This issues a warning to the user, in case they're expecting
-	// to receive data at a higher rate than what is technically possible.
-	scanTimeDelta := argsParsed.TimeDeltaMilliseconds
-	estimatedTimePerScan := 66
-	if scanTimeDelta < int(estimatedTimePerScan) {
-		logger.Warnf("the expected scan rate of deltaT=%v is too small, has to be at least %v", scanTimeDelta, estimatedTimePerScan)
-	}
-
-	return savePCDFiles(ctx, myRobot, rplidar, argsParsed.TimeDeltaMilliseconds, logger)
+	return savePCDFiles(ctx, myRobot, rplidar, scanTimeDelta, logger)
 }
 
 func savePCDFiles(ctx context.Context, myRobot robot.LocalRobot, rplidar camera.Camera, timeDeltaMilliseconds int, logger golog.Logger) (err error) {
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,19 +2,15 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	"go.viam.com/rdk/services/web"
 	"go.viam.com/rplidar"
+	"go.viam.com/rplidar/cmd/helper"
 
 	"github.com/edaniels/golog"
 	"go.viam.com/rdk/config"
-	"go.viam.com/rdk/metadata/service"
 	"go.viam.com/rdk/rlog"
-	"go.viam.com/rdk/usb"
 	"go.viam.com/utils"
 
 	"go.viam.com/rdk/grpc/client"
@@ -47,66 +43,29 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 	if err := utils.ParseFlags(args, &argsParsed); err != nil {
 		return err
 	}
+	argsParsed.Port = helper.GetPort(argsParsed.Port, utils.NetPortFlag(defaultPort), logger)
 
-	if argsParsed.Port == 0 {
-		logger.Debugf("using default port %d ", defaultPort)
-		argsParsed.Port = utils.NetPortFlag(defaultPort)
-	} else {
-		logger.Debugf("using user defined port %d ", argsParsed.Port)
-	}
-
-	devicePath := argsParsed.DevicePath
-	if devicePath == "" {
-		usbDevices := usb.Search(
-			usb.SearchFilter{},
-			func(vendorID, productID int) bool {
-				return vendorID == rplidar.USBInfo.Vendor && productID == rplidar.USBInfo.Product
-			})
-
-		if len(usbDevices) != 0 {
-			logger.Debugf("detected %d lidar devices", len(usbDevices))
-			for _, comp := range usbDevices {
-				logger.Debug(comp)
-			}
-			devicePath = usbDevices[0].Path
-		} else {
-			return errors.New("no usb devices found")
-		}
-	}
-
-	if argsParsed.DataFolder == "" {
-		logger.Debugf("using default data folder '%s' ", defaultDataFolder)
-		argsParsed.DataFolder = defaultDataFolder
-	} else {
-		logger.Debugf("using user defined data folder %s", argsParsed.DataFolder)
-	}
-	if err := os.MkdirAll(filepath.Join(".", argsParsed.DataFolder), os.ModePerm); err != nil {
-		return errors.New("can not create a new directory named: " + argsParsed.DataFolder)
-	}
-
-	// Create rplidar component
-	lidarDevice := config.Component{
-		Name:  name,
-		Type:  config.ComponentTypeCamera,
-		Model: rplidar.ModelName,
-		Attributes: config.AttributeMap{
-			"device_path": devicePath,
-			"data_folder": argsParsed.DataFolder,
-		},
+	lidarDevice, err := helper.CreateRplidarComponent(name,
+		rplidar.ModelName,
+		argsParsed.DevicePath,
+		argsParsed.DataFolder,
+		defaultDataFolder,
+		config.ComponentTypeCamera,
+		logger)
+	if err != nil {
+		return err
 	}
 
 	return runServer(ctx, int(argsParsed.Port), lidarDevice, logger)
 }
 
-func runServer(ctx context.Context, port int, lidarComponent config.Component, logger golog.Logger) (err error) {
-
-	metadataSvc, err := service.New()
+func runServer(ctx context.Context, port int, lidarDevice config.Component, logger golog.Logger) (err error) {
+	ctx, err = helper.GetServiceContext(ctx)
 	if err != nil {
 		return err
 	}
-	ctx = service.ContextWithService(ctx, metadataSvc)
 
-	cfg := &config.Config{Components: []config.Component{lidarComponent}}
+	cfg := &config.Config{Components: []config.Component{lidarDevice}}
 	myRobot, err := robotimpl.New(ctx, cfg, logger, client.WithDialOptions(rpc.WithInsecure()))
 	if err != nil {
 		return err
